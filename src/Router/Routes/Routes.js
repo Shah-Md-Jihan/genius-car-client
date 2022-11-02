a374749c44da95939d05c74e721244bddba388a4
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-11-02 09:31:50 +0600

    project configuration and and layout added

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,17 @@
+import Main from "../../Layouts/Main";
+import Home from "../../Pages/Home/Home";
+const { createBrowserRouter } = require("react-router-dom");
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [{
+            path: '/',
+            element: <Home></Home>
+        }]
+    }
+]);
+
+export default routes;
